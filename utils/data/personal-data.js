a7cf20903fd4d313fb39baca28734317b8041c01
JKL90 <167874464+JKL90@users.noreply.github.com>
2026-02-24 11:49:34 +0100

    Ajout de profile

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Jordan LOKO",
   profile: '/profile.jpg',
-  designation: "Développeur full-stack",
+  designation: "Développeur full-stack Designer UI/UX",
   description: "Mon nom est Jordan K.L. LOKO. Je suis un programmeur professionnel et passionné dans mon quotidien. J'apprends rapidement et j’adopte toujours une attitude autodidacte. J’aime découvrir et explorer de nouvelles technologies, et je suis animé par la résolution de problèmes. J’apprécie pratiquement toutes les étapes du développement d’applications web et je souhaite rendre le web encore plus accessible au monde. Mes compétences principales reposent sur JavaScript, et j’aime réaliser la plupart de mes projets avec ce langage. ",
   email: 'jkl90dev@gmail.com',
   phone: '+229 01 57 94 08 01',
